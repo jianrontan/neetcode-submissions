6dec79b23bc661c2da3c966895ed04db298da09e
Tan Jian Ron <116509027+jianrontan@users.noreply.github.com>
2026-04-15 11:17:40 +0800

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def sub(arr: List[int], idx: int) -> None:
+            res.append(arr[:])
+            print("arr added:", arr[:])
+            for i in range(idx, len(nums)):
+                if i > idx and nums[i] == nums[i - 1]:
+                    continue
+                arr.append(nums[i])
+                sub(arr[:], i + 1)
+                arr.pop()
+        
+        sub([], 0)
+        return res
