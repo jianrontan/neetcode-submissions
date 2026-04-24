ed00637f338c7db44fceb4a77f3192555e6c55e8
Tan Jian Ron <116509027+jianrontan@users.noreply.github.com>
2026-04-24 13:07:21 +0800

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            lenS = len(s)
+            res += str(lenS) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        numStr, num = "", 0
+        i = 0
+        while i < len(s):
+            if s[i] == "#":
+                num = int(numStr)
+                # we have 3#abc then we need index 2 (i+1) to 4 (i+1+num+1)
+                res.append(s[i + 1:i + 1 + num])
+                i = i + 1 + num
+                numStr, num = "", 0
+            else:
+                numStr += s[i]
+                i += 1
+        return res
