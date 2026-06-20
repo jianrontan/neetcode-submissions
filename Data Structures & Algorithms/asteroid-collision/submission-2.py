b509a7e954a6d474cdf9bcec32d9eea80e2fc015
Tan Jian Ron <116509027+jianrontan@users.noreply.github.com>
2026-06-20 19:38:51 +0800

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        res = []
+        
+        for asteroid in asteroids:
+            while res and asteroid < 0 < res[-1]:
+                if res[-1] < abs(asteroid):
+                    res.pop()
+                    continue
+                elif res[-1] == abs(asteroid):
+                    res.pop()
+                break
+            else:
+                res.append(asteroid)
+        
+        return res
