621ecaaf8ec08278852fe6e08b12e05207e68327
Tan Jian Ron <116509027+jianrontan@users.noreply.github.com>
2026-06-25 18:53:46 +0800

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+            
+        parent = [i for i in range(n)]
+        rank = [1 for i in range(n)]
+
+        def find(node):
+            if parent[node] == node:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(x, y):
+            rootX, rootY = find(x), find(y)
+            if rootX == rootY:
+                return False
+            elif rank[rootX] > rank[rootY]:
+                parent[rootY] = rootX
+                rank[rootX] += 1
+                return True
+            elif rank[rootX] < rank[rootY]:
+                parent[rootX] = rootY
+                rank[rootY] += 1
+                return True
+            else:
+                parent[rootY] = rootX
+                rank[rootX] += 1
+                return True
+        
+        nodes = n
+        for edge in edges:
+            if not union(edge[0], edge[1]):
+                return False
+            else:
+                nodes -= 1
+
+        if nodes != 1:
+            return False
+
+        return True
