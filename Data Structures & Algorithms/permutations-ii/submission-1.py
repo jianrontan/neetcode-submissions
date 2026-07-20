72327046eac2518c6a537ffd0259530fe3d14cce
Tan Jian Ron <116509027+jianrontan@users.noreply.github.com>
2026-07-20 22:11:08 +0800

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            unique = set()
+            for i in range(len(nums)):
+                if used[i] or nums[i] in unique:
+                    continue
+                unique.add(nums[i])
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path, used)
+                path.pop()
+                used[i] = False
+        
+        backtrack([], used = [False for _ in range(len(nums))])
+        
+        return res
